2884a926f5bf50955514e13ea2a0c60143dd5865
slv922 <slv922@gmail.com>
2023-11-22 22:17:48 +0800

    Update MyFooter.vue

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -19,7 +19,7 @@
 				魔物資料來源: <a target="_blank" href="https://mhnow.cc/">[MHNow 魔物地圖 Monster Hunter Now]</a>
 			</p>
 			<p>
-				Github: https://github.com/slv922/mhnow-map
+				Github: <a target="_blank" href="https://github.com/slv922/mhnow-map">https://github.com/slv922/mhnow-map</a>
 			</p>
 		</div>
 <!-- 
