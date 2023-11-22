c20a70ffbe8b0d68a11c87c19454acbcda9af0f0
slv922 <slv922@gmail.com>
2023-11-22 17:05:52 +0800

    修改網站title

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/mask-map/'
+    ? '/'
     : '/',
     pages: {
         index: {
             entry: './src/main.js',
-            title: '口罩即時查',
+            title: '黑角龍魔物地圖即時查',
         },
     },
 };
