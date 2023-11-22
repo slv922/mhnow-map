224daeee698ca79835b7fbe282c5157f987f850b
slv922 <slv922@gmail.com>
2023-11-22 22:19:18 +0800

    remove unused code

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,14 +1,5 @@
 <template>
   <footer class="footer">
-    <!-- <ul class="footer-contact list list-horizontal list-seperate title-ttr">
-			<li class="list-item">
-				防疫專線 1922
-			</li>
-
-			<li class="list-item">
-				口罩資訊 1919
-			</li>
-		</ul> -->
 
 		<div class="footer-thanks text-sm">
 			<p>
@@ -22,12 +13,7 @@
 				Github: <a target="_blank" href="https://github.com/slv922/mhnow-map">https://github.com/slv922/mhnow-map</a>
 			</p>
 		</div>
-<!-- 
-		<p class="footer-signature text-sm">
-			<a href="https://github.com/slv922/mhnow-map" target="_blank" class="github-link">
-				slv922/mhnow-map
-			</a>
-		</p> -->
+
 
   </footer>
 </template>
